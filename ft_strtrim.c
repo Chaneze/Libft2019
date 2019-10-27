a4a531e476d206ce9d9633987f80aac75f4da272
Chaneze AROUA <charoua@e2r13p9.42.fr>
2019-10-27 19:41:34 +0100

    correction strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: charoua <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 12:01:11 by charoua           #+#    #+#             */
-/*   Updated: 2019/10/27 19:26:36 by charoua          ###   ########.fr       */
+/*   Updated: 2019/10/27 19:40:53 by charoua          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,17 @@ char		*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	beg;
 	size_t	end;
-	char	*trim;
 
 	beg = 0;
 	if (!s1)
-		return (trim = ft_strdup("\0"));
+		return (ft_strdup("\0"));
 	if (!set)
-		return (trim = ft_strdup(s1));
+		return (ft_strdup(s1));
 	end = ft_strlen(s1) - 1;
 	while (s1[beg] && ft_check_set(s1[beg], set))
 		beg++;
 	if (s1[beg] == '\0')
-		return (trim = ((char *)s1) + beg);
+		return (ft_strdup("\0"));
 	while (ft_check_set(s1[end], set))
 		end--;
 	return (ft_substr(s1, beg, (end - beg + 1)));
